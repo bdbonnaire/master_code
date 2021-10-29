bc155e36870c3c38d9c23822703bda502e6856c1
bdb <bdbonnaires@gmail.com>
2021-10-29 16:52:57 +0200

    updated main accordingly

--- c/lab3/ex3/src/main.cpp
+++ c/lab3/ex3/src/main.cpp
@@ -24,13 +24,32 @@ using namespace std;
 int main(){
 
 	MyCollection<int> collec;
-	init(collec, 5);
+	init(collec, 5,  5, 15);
 
-	cout << "Creation of an integer collection of size 5 with random numbers:\n" <<
+	cout << "Creation of an integer collection of size 5 with random numbers from 5 to 15:\n" <<
 		collec;
 
-	MyCollection<int> res;
-	apply_fact<int>(collec, res);	
+	MyCollection<char> res;
+	apply_fact<char>(collec, res);	
 	cout << "Here is the factorial of every elements in the previous collection :\n" << res << endl;
 
+	MyCollection<short int> res1;
+	apply_fact<short int>(collec, res1);	
+	cout << "Here is the factorial of every elements in the previous collection :\n" << res << endl;
+
+	MyCollection<int> res2;
+	apply_fact<int>(collec, res2);	
+	cout << "Here is the factorial of every elements in the previous collection :\n" << res << endl;
+
+	MyCollection<long int> res3;
+	apply_fact<long int>(collec, res3);	
+	cout << "Here is the factorial of every elements in the previous collection :\n" << res << endl;
+
+	MyCollection<long long int> res4;
+	apply_fact<long long int>(collec, res4);	
+	cout << "Here is the factorial of every elements in the previous collection :\n" << res << endl;
+
+	MyCollection<__int128> res5;
+	apply_fact<__int128>(collec, res5);	
+	cout << "Here is the factorial of every elements in the previous collection :\n" << res << endl;
 }
