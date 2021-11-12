1fc22de5dbe9b2948241f6afc15850dc1b9a0095
bdb <bdbonnaires@gmail.com>
2021-11-12 19:18:50 +0100

    added a few comments to make everything clearer

--- sp/lab4/ex1/ex1.py
+++ sp/lab4/ex1/ex1.py
@@ -15,17 +15,21 @@ def hamming_win(x, N):
 def plot_Hamming_rect_fft(N, ax):
 
     n = np.arange(0, N)
+    #creation of the rectangular window
     rect = np.ones(N)
     #Normalisation with norm l1
     rect_norm = rect / np.linalg.norm(rect, ord=1)
 
+    #creation of the Hamming window
     hamming = hamming_win(n, N)
     # Normalisation with norm l1
     hamming_norm = hamming / np.linalg.norm(hamming, ord=1)
 
     # Calculating the fft and shifting it
+    # d=L tells the fft to pad the signal with 0 so that L values are returned
     hamming_fft = np.fft.fft(hamming_norm, n=L)
     hamming_fft = np.fft.fftshift(hamming_fft)
+
     rect_fft = np.fft.fft(rect_norm, n=L)
     rect_fft = np.fft.fftshift(rect_fft)
 
@@ -36,10 +40,10 @@ def plot_Hamming_rect_fft(N, ax):
     ax.set_xlabel("Frequency bins")
     ax.set_ylabel("Normalized Magnitude")
 
+
 plot_Hamming_rect_fft(N[0], axes[0])
 plot_Hamming_rect_fft(N[1], axes[1])
 plot_Hamming_rect_fft(N[2], axes[2])
 plt.legend()
 
 plt.show(block=False)
-#plt.savefig("comparison.svg", bbox_inches='tight')
