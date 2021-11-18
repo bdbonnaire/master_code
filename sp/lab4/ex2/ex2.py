310cf511a975e93325b2d262d74a05d94a1a8f59
bdb <bdbonnaires@gmail.com>
2021-11-18 12:42:27 +0100

    Fixed typos and added labels

--- sp/lab4/ex2/ex2.py
+++ sp/lab4/ex2/ex2.py
@@ -5,6 +5,9 @@ import matplotlib.pyplot as plt
 # the coefficients of h when N is even
 hn_tilda = lambda n, f0 : 2*f0*np.sinc(2*f0*(n-1/2))
 
+# number of frequency bins
+L = 1024
+
 def f_h(f0, N):
     """
     Calculates the fourier transform of the filter h. 
@@ -18,7 +21,7 @@ def f_h(f0, N):
         
     n = np.arange(-N//2, N//2)
     H = hn_tilda(n, f0)
-    S = fft.fft(H, 1024)
+    S = fft.fft(H, L)
     S = fft.fftshift(S)
     
     return S
@@ -33,6 +36,8 @@ def finite_impulse_response(f0, N):
     Returns :
         g : ndarray = fourier coefficients of the finite impulse response.
     """
+    hn_tilda = lambda n, f0 : 2*f0*np.sinc(2*f0*(n-1/2))
+
     n = np.arange(-N//2, N//2)
     h = hn_tilda(n, f0)
 
@@ -43,18 +48,26 @@ def finite_impulse_response(f0, N):
 
     return g
 
-N=100
+################ Plotting #############################
+fig, axes = plt.subplots(1, 3)
+fig.suptitle("Comparison between rectangular and Hamming window with $N$ coefficients", fontsize=16)
+N=[10, 100, 200]
 f0 = 1/4
-g = finite_impulse_response(f0, N)
-f_g = fft.fft(g, 1024)
-f_g = fft.fftshift(f_g)
+for k in N:
+    g = finite_impulse_response(f0, k)
+    f_g = fft.fft(g, L)
+    f_g = fft.fftshift(f_g)
 
-S = f_h(f0, N)
-n = np.arange(-1024//2, 1024//2) / 1024
+    S = f_h(f0, k)
+    n = np.arange(-L//2, L//2) / L
 
-plt.plot(n,abs(f_g), label="Hamming_window")
-plt.plot(n, abs(S), label="rectangular window")
-plt.axvline(-f0, color='C7', ls='--')
-plt.axvline(f0, color='C7', ls='--', label=r"$\pm f_0$")
+    axes[N.index(k)].set_title(r"$N = $" + str(k))
+    axes[N.index(k)].plot(n,abs(f_g), label="Hamming window")
+    axes[N.index(k)].plot(n, abs(S), label="rectangular window")
+    axes[N.index(k)].axvline(-f0, color='C7', ls='--')
+    axes[N.index(k)].axvline(f0, color='C7', ls='--', label=r"$\pm f_0$")
+
+axes[1].set_xlabel("frequencies")
+axes[0].set_ylabel("Magnitude")
 plt.legend();
 plt.show();
