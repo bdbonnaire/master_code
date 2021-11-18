b6f13d821824039bd7f54f3fa8c02b6ede3ec15e
bdb <bdbonnaires@gmail.com>
2021-11-18 12:43:27 +0100

    finished ex3

--- sp/lab4/ex3/ex3.py
+++ sp/lab4/ex3/ex3.py
@@ -12,9 +12,13 @@ def finite_impulse_response(f0, N):
     Returns :
         g : ndarray = fourier coefficients of the finite impulse response.
     """
-    hn_tilda = lambda n, f0 : 2*f0*np.sinc(2*f0*(n-1/2))
+    if N // 2 == 0 :
+        hn_tilda = lambda n, f0 : 2*f0*np.sinc(2*f0*(n-1/2))
+        n = np.arange(-N//2, N//2)
+    else:
+        hn_tilda = lambda n, f0 : 2*f0*np.sinc(2*f0*n)
+        n = np.arange(-(N-1)//2, (N+1)//2)
 
-    n = np.arange(-N//2, N//2)
     h = hn_tilda(n, f0)
 
     w_h = lambda n : .54 - .46 * np.cos(2 * np.pi * n/N) 
@@ -35,9 +39,17 @@ H = fft.fftshift(H)
 
 N = np.arange(-1024//2, 1024//2) / 1024
 
-plt.axvline(f1 + f0)
-plt.axvline(-(f1 + f0))
-plt.axvline(f0)
-plt.axvline(-f0)
-plt.plot(N, np.abs(H))
+########################### Plotting #####################################
+# vertical lines
+plt.axvline(f1 + f0, linestyle ='--', color='C7')
+plt.axvline(-(f1 + f0), linestyle ='--', color='C7')
+plt.axvline(f0, linestyle ='--', color='C7')
+plt.axvline(-f0, linestyle ='--', color='C7', label =r"$\pm f_0 and \pm f_0 + f_1")
+
+#bandpass filter
+plt.plot(N, np.abs(H), label="Band-pass filter")
+plt.suptitle("Band-pass filter using Hamming Window", fontsize=16)
+plt.title(r"50 coefficients kept, $f_0 = \frac{1}{8}$ and $f_1 = \frac{1}{4}$")
+plt.xlabel("frequency")
+plt.ylabel("Magnitude")
 plt.show()
