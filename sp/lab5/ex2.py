1ebb10c8b0db196518c94d666c0f9d1bac171864
bdb <bdbonnaires@gmail.com>
2021-11-29 10:29:01 +0100

    finished ex2

--- sp/lab5/ex2.py
+++ sp/lab5/ex2.py
@@ -1,16 +1,18 @@
 import numpy as np
 import matplotlib.pyplot as plt
+from matplotlib import ticker
 from matplotlib.patches import Rectangle
 import scipy.fftpack as fftp
 
 
 
 # import the cameraman image using pyplot.
-# image is a 256x256 ndarray where each value represents the pixel
-# of the picture and its contrast from 0 to 1.
+# image is a 256x256 ndarray where each value represents pixels
+# in the picture and its contrast from 0 to 1.
 image = plt.imread("cameraman.png")
 L = 256
 
+####################### Application of the DCT #########################
 dct_image = np.zeros((L,L))
 
 for i in np.arange(0,L,8):
@@ -37,12 +39,27 @@ def quantize(q, dct_image):
 # counts the occurence of 0 in the dct
     ratio = 0
     for i in dct_image.ravel():
-        if i >= 1E-5 :
+        if i <= 1E-5 :
             ratio += 1
 
-# calculates the ratio per the size of the image L²
+# calculates the ratio with the size of the image L²
     ratio /= L**2
     return ratio, dct_image
+
+q_val = np.arange(20)
+q_ratio = np.zeros(20)
+for i in range(20):
+    q_ratio[i] = quantize(q_val[i], dct_image)[0]
+
+################ Plot of the Ratio #############################
+fig, ax = plt.subplots()
+ax.plot(q_val, q_ratio)
+ax.xaxis.set_major_locator(ticker.MultipleLocator(2))
+ax.yaxis.set_major_formatter(ticker.PercentFormatter(1))
+ax.set_xlabel("Values of q")
+ax.set_ylabel("Ratio of zeros")
+
+plt.show()
 
 
 ################ Plot of the Comparison ##############################
