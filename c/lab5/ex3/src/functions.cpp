50df095143ab5c9605a36a2e82399831da4953c3
bdb <bdbonnaires@gmail.com>
2021-11-29 20:10:07 +0100

    cpp/lab5/ex3 finished poly0

--- c/lab5/ex3/src/functions.cpp
+++ c/lab5/ex3/src/functions.cpp
@@ -28,3 +28,72 @@ void Function::print(std::ostream& o)
 {
 	o << name << "(x) = ";
 }
+
+//////////////////// Poly0 Class //////////////////
+
+Poly0::Poly0(std::string n, float c0) :
+	Function(n),
+	a0(c0)
+{}
+
+double Poly0::eval(double x0)
+{
+	return a0;
+}
+
+Function* Poly0::derivative()
+{
+	return new Poly0(name + "'", 0);
+}
+
+void Poly0::print(std::ostream& o)
+{
+	Function::print(o);
+	o << a0 << std::endl;
+}
+
+//////////////////// Poly1 Class //////////////////
+
+Poly0::Poly0(std::string n, float c0) :
+	Function(n),
+	a0(c0)
+{}
+
+double Poly0::eval(double x0)
+{
+	return a0;
+}
+
+Function* Poly0::derivative()
+{
+	return new Poly0(name + "'", 0);
+}
+
+void Poly0::print(std::ostream& o)
+{
+	Function::print(o);
+	o << a0 << std::endl;
+}
+
+//////////////////// Poly0 Class //////////////////
+
+Poly0::Poly0(std::string n, float c0) :
+	Function(n),
+	a0(c0)
+{}
+
+double Poly0::eval(double x0)
+{
+	return a0;
+}
+
+Function* Poly0::derivative()
+{
+	return new Poly0(name + "'", 0);
+}
+
+void Poly0::print(std::ostream& o)
+{
+	Function::print(o);
+	o << a0 << std::endl;
+}
